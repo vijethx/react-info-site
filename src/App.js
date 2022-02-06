b3d39820ee110b9cd1c4686311f15f9b9ff3ac63
vijethx <38877972+vijethx@users.noreply.github.com>
2022-02-06 19:56:02 +0530

    added light mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 export default function App() {
-    return (
-        <div className="container">
-            <Navbar />
-            <Main />
-        </div>
-    )
+	const [darkMode, setDarkMode] = React.useState(true);
+	function toggleDarkMode() {
+		setDarkMode(!darkMode);
+	}
+
+	return (
+		<div className="container">
+			<Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+			<Main darkMode={darkMode} />
+		</div>
+	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
-import React from "react"
-import reactLogo from '../images/react-icon-small.png'
-export default function Navbar() {
-    return (
-        <nav>
-            <img src={reactLogo} className="nav--icon" alt="react logo" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
-    )
+import React from "react";
+
+export default function Navbar(props) {
+	return (
+		<nav className={props.darkMode ? "dark" : ""}>
+			<img
+				className="nav--logo_icon"
+				src="./images/react-icon-small.png"
+				alt=" "
+			/>
+			<h3 className="nav--logo_text">ReactFacts</h3>
+
+			<div className="toggler">
+				<p className="toggler--light">Light</p>
+				<div className="toggler--slider" onClick={props.toggleDarkMode}>
+					<div className="toggler--slider--circle"></div>
+				</div>
+				<p className="toggler--dark">Dark</p>
+			</div>
+		</nav>
+	);
 }
